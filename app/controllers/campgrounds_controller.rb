5f02cc53eda962cd805a85d1897352d6cf5c69a4
Stephen Advincula <dev@sylv3rblade.com>
2018-03-15 12:07:19 +0800

    WIP form

--- app/controllers/campgrounds_controller.rb
+++ app/controllers/campgrounds_controller.rb
@@ -7,4 +7,18 @@ class CampgroundsController < ApplicationController
         paid.
         default_query.includes(:city)
   end
+
+  def new
+    @campground = Campground.new
+  end
+
+  def create
+    # TODO handling for validation
+    campground = Campground.create(campground_params)
+    redirect_to action: :index
+  end
+
+  def campground_params
+    params.require(:campground).permit(:city_id, :name, :public_park, :cost_per_night)
+  end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,7 @@
 class City < ApplicationRecord
   has_many :campgrounds, dependent: :destroy
 
+  scope :default_query, -> { limit(20).order(name: 'asc') }
+
   validates :name, presence: true
 end
