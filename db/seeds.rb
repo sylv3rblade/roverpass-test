219bd6f4857cfcce94c8a68e44e642b99151d905
Stephen Advincula <dev@sylv3rblade.com>
2018-03-15 10:04:25 +0800

    simple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Campgrounds in Anchorage
+anchorage = City.create(name: 'Anchorage')
+austin = City.create(name: 'Austin')
+
+
+200.times.each do
+  begin
+    Campground.create!(
+      name: "#{Faker::Company.name} campground",
+      public_park: [true, false].sample,
+      city: [anchorage, austin].sample,
+      cost_per_night: [10, 20, 50, 70, 100, 150].sample
+    )
+  rescue
+  end
+end
+
+
+puts 'done populating campgorunds'
+
